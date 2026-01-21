361c8cc6d235bde2916b199b58ecfc648dd4eec4
eunho <76768140sa@gmail.com>
2026-01-22 00:13:06 +0900

    feat: config 파일 추가

---
+++ src/main/java/com/example/springbatch/config/MetaDBConfig.java
@@ -0,0 +1,34 @@
+package com.example.springbatch.config;
+
+import org.springframework.boot.autoconfigure.batch.BatchDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class MetaDBConfig {
+
+    /*
+     *  @Primary                 : DB 우선 순위 결정
+     *  @ConfigurationProperties : application.yml에 있는 DB설정 가져옴
+     */
+    @Primary
+    @Bean
+    @BatchDataSource
+    @ConfigurationProperties(prefix="spring.datasource-meta")
+    public DataSource metaDBSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Primary
+    @Bean
+    public PlatformTransactionManager metaTransactionManager() {
+        return new DataSourceTransactionManager(metaDBSource());
+    }
+}
